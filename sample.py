d67d0eeff28313690f0ab10e0a0310505830b3b8
ang.a <ang.a@bm.technion.ac.il>
2024-04-01 14:21:11 +0300

    added skimage to reqs

--- sample.py
+++ sample.py
@@ -1,6 +1,8 @@
 from ivim_morph.infer import load_case, infer_from_data
 import os
 import torch 
+import matplotlib.pyplot as plt
+import numpy as np
 
 path = '//tcmldrive/NogaK/fetal_lung_case'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -12,5 +14,3 @@ signal, seg = load_case(data_path, seg_path, device=device)
 bvalues = [0, 50, 100, 200, 400, 600]
 
 Dp, D, f = infer_from_data(signal, seg, bvalues, device=device)
-
-print(Dp, D, f)
